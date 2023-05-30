3f58243bb0e4622a68e8307cbc3765db7db3d925
Neurolius <ewgensafonow@gmail.com>
2023-05-30 13:12:01 +0800

    names

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,7 +30,7 @@
 
 <nav>
     <div class="nav-container">
-        <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
+        <a href="/" class="nav-logo" title="Back to Homepage">Сафонов Евгений</a>
         <div class="nav-links">
             {#each nav as link}
                 {#if link.isShow}
